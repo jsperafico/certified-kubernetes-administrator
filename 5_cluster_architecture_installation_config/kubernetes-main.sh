29daff01fefa71b86d938a632cc8ee4eb4f9b76f
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2024-05-01 18:04:25 +0300

    kubeadmin cidr compatible with kube flannel

--- 5_cluster_architecture_installation_config/kubernetes-main.sh
+++ 5_cluster_architecture_installation_config/kubernetes-main.sh
@@ -30,7 +30,7 @@ sudo netplan apply
 
 sleep $((120 - $(date +%S) ))
 
-kubeadm init
+kubeadm init --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
