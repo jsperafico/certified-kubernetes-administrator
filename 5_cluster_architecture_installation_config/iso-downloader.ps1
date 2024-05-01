c5a80363b66e971dbb3484e0318285fbd0c05274
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2024-05-01 13:19:03 +0300

    reliable url to download ubuntu server

--- 5_cluster_architecture_installation_config/iso-downloader.ps1
+++ 5_cluster_architecture_installation_config/iso-downloader.ps1
@@ -7,9 +7,9 @@ Param (
     [string]$FileName,
     
     [Parameter()]
-    [string]$Url = "https://www.nic.funet.fi/pub/mirrors/releases.ubuntu.com/22.04/ubuntu-22.04.4-live-server-amd64.iso"
+    [string]$Url = "https://releases.ubuntu.com/22.04/ubuntu-22.04.4-live-server-amd64.iso"
 )
 
 $IsoPath = "$Path\$FileName.iso"
 
-Invoke-WebRequest -Uri $Url -OutFile $IsoPath
+Invoke-WebRequest -Uri $Url -OutFile $IsoPath  -TimeoutSec 600
